680595211e37e3b4d90bb44649afe331ed906795
Patrick Roberts <patrob22@gmail.com>
2023-03-20 14:11:40 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+};
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street)*264;
+};
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs((end - start)*264);
+};
+
+function calculatesFarePrice(start, end) {
+    let dist = distanceTravelledInFeet(start, end);
+    if (dist <= 2500) {
+        if (dist <= 2000) {
+            if (dist <=400) {
+                return 0;
+            }
+            return (dist - 400)*0.02;
+        }
+        return 25;
+    } else {
+        return "cannot travel that far"
+    };
+};
